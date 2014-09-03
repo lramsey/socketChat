fe93cd8d588cab68f90557c61f4cc919f4bf994d
lramsey <lramsey177@gmail.com>
2014-09-02 17:14:32 -0700

    socket chat room and native dom, username, chat, and usercount enabled

---
+++ index.js
@@ -0,0 +1,44 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(3000, '127.0.0.1');
+console.log('listening on port 3000');
+function handler(req, res){
+  fs.readFile(__dirname + '/index.html', function(err, data){
+    if(err){
+      console.log('failure to read file:', err);
+    }
+    else {
+      var file = data;
+      res.writeHead(200);
+      res.end(file);
+    }
+  });
+}
+var i = 0;
+var id = 0;
+var connections = [];
+io.on('connection', function(socket){
+  console.log('connection made',++i);
+  id++;
+  var index = connections.push(socket);
+  
+  io.emit('new', {number: i, id: id});
+  
+  socket.on('post', function(data){
+    var post = data.post;
+    console.log('message received:', data.post);
+    socket.broadcast.emit('post', data);
+  });
+  socket.on('my other event', function(data){
+    console.log('yo:', data.my);
+  });
+  socket.on('disconnect', function(data){
+    connections.splice(index, 1);
+    console.log('connection lost:', --i);
+    
+    io.emit('new', {number: i});
+  });
+
+});
